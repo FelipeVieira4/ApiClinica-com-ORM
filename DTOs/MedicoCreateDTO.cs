cc1cc98537f0d8224a2063d452e9b7461af4190f
Klaus-E-J <klaus.jourdain@gmail.com>
2026-04-26 21:18:04 -0300

    Partial implementation of 'Medicos'. Still lacking Controller.

--- DTOs/MedicoCreateDTO.cs
+++ DTOs/MedicoCreateDTO.cs
@@ -2,16 +2,14 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ApiClinica.DTOs;
 
-public class PacienteCreateDTO
+public class MedicoCreateDTO
 {
     public required string Nome { get; set; }
 
-    [EmailAddress(ErrorMessage = "Email inválido")]
+    [EmailAddress(ErrorMessage = "Email invalido")]
     public required string Email { get; set; }
 
     public required string Telefone { get; set; }
 
-    public required DateOnly DataNasc { get; set; }
-
-    public required string Cpf { get; set; }
+    public required string CRM { get; set; }
 }
